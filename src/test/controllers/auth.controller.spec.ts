9598d747ff9d1428ffbd386b2162c1598b3b3ebb
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2022-09-21 17:20:20 -0500

    now that we decided to use getByEmail() mocking for endpoint testing it is safe to use the same mocking for the login() tests

--- src/test/controllers/auth.controller.spec.ts
+++ src/test/controllers/auth.controller.spec.ts
@@ -1,12 +1,25 @@
 import { ClientInfo } from "../../models/refresh-token";
 import { login } from "../../controllers/auth.controller";
+import { UserModel } from '../../models/user';
 
 describe('controllers/auth.controller', () => {
     describe('login', () => {
+        beforeEach(async () => {
+            // mock UserModel methods
+            jest.spyOn(UserModel, 'getByEmail')
+                .mockImplementation((email) => Promise.resolve({ 
+                    email, 
+                    password: 'some-password', 
+                    toJSON: () => ({ email, password: 'some-password' }) 
+                }) as never)
+        })
+
         it('should return something', async () => {
             const result = await login('user@example.com', 'some-password', {} as ClientInfo);
             console.log(result);
             expect(result).toBeDefined();
         });
     });
+
+    afterEach(() => { jest.clearAllMocks() });
 });
