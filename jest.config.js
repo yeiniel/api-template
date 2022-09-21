a781289db60b2add916a95b637841edbfc74903c
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2022-09-21 15:33:11 -0500

    update jest config to only cover typescript files
    fix issue #2

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['**/src/**/*'],
+  collectCoverageFrom: ['**/src/**/*.ts'],
 };
