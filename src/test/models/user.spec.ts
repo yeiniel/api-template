b06449b4072f93c931477e9684c176faac8c981b
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2022-09-20 22:01:51 -0500

    provide test for error error TS2305: Module '"../models/user"' has no exported member 'User'
    There is a missing export on the user model. A sinple existence test is added for the User symbol

---
+++ src/test/models/user.spec.ts
@@ -0,0 +1,9 @@
+import { User } from '../../models/user';
+
+describe('models/user', () => {
+    describe('User', () => {
+        it('should exist', () => {
+            expect(User).toBeDefined();
+        })
+    });
+});
