75b9b2b36ed9eda7a4590a52195507f339438863
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2022-09-21 01:13:45 -0500

    provide test for src/controllers/auth.controller.ts:36:42 - error TS2339: Property 'getByEmail' does not exist on type 'ReturnModelType<typeof User, BeAnObject>'

--- src/test/models/user.spec.ts
+++ src/test/models/user.spec.ts
@@ -1,9 +1,23 @@
-import { User } from '../../models/user';
+import { User, UserModel } from '../../models/user';
 
 describe('models/user', () => {
     describe('User', () => {
         it('should exist', () => {
             expect(User).toBeDefined();
-        })
+        });
+    });
+
+    describe('UserModel', () => {
+        describe('getByEmail', () => {
+            it('should work', () => {
+                const findOneSpy = jest.spyOn(UserModel, 'findOne').mockImplementation(() => Promise.resolve({}) as any);
+
+                const result = UserModel.getByEmail('user@demo.com');
+
+                expect(findOneSpy).toHaveBeenCalledTimes(1);
+                expect(findOneSpy.mock.calls[0][0].email).toBe('user@demo.com');
+                expect(result).toBe(findOneSpy.mock.results[0].value);
+            });
+        });
     });
 });
