8933a8ee5c1c14972dffba0a1111f7f7db30d653
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2022-09-21 19:31:53 -0500

    avoid crashing the app at start if MongoDB is offline
    fix issue #3

--- src/app.local.ts
+++ src/app.local.ts
@@ -1,11 +1,7 @@
 // import express from 'serverless-express/express';
 import app from './app';
-import MongoService from './lib/mongodb';
 
-// Call to app.listen to create local server for dev env.
-MongoService.getConnection().then((connection) => {
-  const port = parseInt(process.env['port'], 10) || 3300;
-  app.listen(port, '0.0.0.0');
+const port = parseInt(process.env['port'], 10) || 3300;
+app.listen(port, '0.0.0.0');
 
-  console.log('App listenting on :', port);
-});
+console.log('App listenting on :', port);
