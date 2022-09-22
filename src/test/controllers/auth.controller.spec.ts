ed39343a29cf7e45de02fd4e0437a20537b9b840
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2022-09-21 23:12:04 -0500

    move credential checking into the user model and out of the auth controller
    The way how user credentials can be checked is business model specific

--- src/test/controllers/auth.controller.spec.ts
+++ src/test/controllers/auth.controller.spec.ts
@@ -1,20 +1,31 @@
 import { ClientInfo } from "../../models/refresh-token";
 import { login } from "../../controllers/auth.controller";
-import { UserModel } from '../../models/user';
+import { User, UserModel } from '../../models/user';
 
 describe('controllers/auth.controller', () => {
     describe('login', () => {
+        let email: User['email'];
+        let password: User['password'];
+
         beforeEach(async () => {
+            email = `user-${Math.floor(Math.random() * 100)}@example.com`;
+            password = `some-password-${Math.random()}`;
+
+            // setup some envs used by createToken method
+            process.env['JWT_SECRET'] = 'some-secret';
+            process.env['TOKEN_EXPIRES_IN'] = '1h';
+
             // mock UserModel methods
-            jest.spyOn(UserModel, 'getByEmail')
-                .mockImplementation((email) => Promise.resolve(new UserModel({ 
-                    email, 
-                    password: 'some-password'
-                })) as never)
+            jest.spyOn(UserModel, 'checkCredentials')
+                .mockImplementation((e, p) => Promise.resolve(
+                    e === email && p === password 
+                        ? { email, password, role: 1 } 
+                        : false
+                ) as never)
         })
 
         it('should return something', async () => {
-            const result = await login('user@example.com', 'some-password', {} as ClientInfo);
+            const result = await login(email, password, {} as ClientInfo);
             expect(result).toBeDefined();
         });
     });
