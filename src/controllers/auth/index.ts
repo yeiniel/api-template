8a36566fd1e28bd79e7db6ff66989908228fcfdd
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2022-09-23 00:55:04 -0500

    refactor forgot-password and reset-password endpoint handling

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,6 @@
+export { RegisterController } from './register.controller';
+export { LoginController } from './login.controller';
+export { RefreshTokenController } from './refresh-token.controller';
+export { ValidateTokenController } from './validate-token.controller';
+export { ForgotPasswordController } from './forgot-password.controller';
+export { ResetPasswordController } from './reset-password.controller';
